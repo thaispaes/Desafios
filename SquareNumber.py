cecbc07e03625a2c9d4764942b9365c1260751e9
Thais Paes <thaisreginapaesdossantos@gmail.com>
2024-01-24 20:29:59 -0300

    Fix: Otimize code

--- SquareNumber.py
+++ SquareNumber.py
@@ -1,13 +1,8 @@
 def is_square(number):   
-    if number < 0:
-        return False
-    elif number == 0 or number == 1:
+    if number == 0 or number == 1:
         return True
     else:
-        for num in range(2, number):  
-            divider = number/num
-            if divider == num:
+        for num in range(2, number): 
+            if number/num == num:
                 return True
         return False
-    
-print(is_square(25))            
