75592ab738505ab386713d995650d96cd6fb5d44
Thais Paes <thaisreginapaesdossantos@gmail.com>
2024-01-24 23:46:51 -0300

    Feat: Desafio 7 pqp

--- Walking.py
+++ Walking.py
@@ -1,18 +1,28 @@
-def verify_duplicates(walk):
-    for direction in range(len(walk)):
-        if walk[direction] == walk[direction+1]:
+def verify_route(walk):
+    north, east = 0, 0
+    if len(walk) == 10:
+        for direction in walk:
+            if direction == 'n': north+=1
+            if direction == 's': north-=1
+            if direction == 'w': east-=1
+            if direction == 'e': east+=1
+        if north == 0 and east == 0:
             return True
-        else:
-            return False
-       
+    else:
+        return False
+    
 def is_valid_walk(walk):
     if walk == []: return False
     
-    
     for direction in walk:
-        if len(walk) == 10 and verify_duplicates(walk) == False:
+        if len(walk) == 10 and verify_route(walk) == True:
             return True 
+        
+        if len(walk) <10 or len(walk) >10:
+            return False
         
         else:
             return False
         
+lista = ['s', 'e', 'w', 'n', 'w', 's', 'e', 'w', 'n', 's']  
+print(is_valid_walk(lista))
